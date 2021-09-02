cf26c21d3342b4294dc78ddbd82379bcb019969b
chulbulsuman224 <77154652+chulbulsuman224@users.noreply.github.com>
2021-09-02 18:01:45 +0530

    Add files via upload

---
+++ CreatePost.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class CreatePost extends React.Component{
+render() {
+    return(
+        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+            <Text>CreatePost</Text>
+        </View>
+    )
+}
+}
+
+
+ 
